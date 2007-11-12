ee0ef20cf74f047e168eb85b403d12ab0f94c735
andreas <andreas@f893cd9c-26d5-0310-a8ce-8029f9ad61c0>
2007-11-12 17:23:33 +0000

    before the merge with the interaction extension --This line, and these below, will be ignored--
    A    http://www.derkholm.net/svn/repos/dasobert/tags/interaction_merge_20071112

---
+++ dasobert/tags/interaction_merge_20071112/doc/examples/GetSequence.java
@@ -0,0 +1,138 @@
+/*
+ *                    BioJava development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ * Copyright for this code is held jointly by the individual
+ * authors.  These should be listed in @author doc comments.
+ *
+ * For more information on the BioJava project and its aims,
+ * or to join the biojava-l mailing list, visit the home page
+ * at:
+ *
+ *      http://www.biojava.org/
+ *
+ * Created on 24.11.2005
+ * @author Andreas Prlic
+ *
+ */
+
+import org.biojava.dasobert.das.SequenceThread;
+import org.biojava.dasobert.dasregistry.Das1Source;
+import org.biojava.dasobert.eventmodel.SequenceEvent;
+import org.biojava.dasobert.eventmodel.SequenceListener;
+
+/** a class that demonstrates how to get a protein structure
+ * from a structure DAS server.
+ */
+
+public class GetSequence {
+
+	public static void main(String[] args) {
+	    String ac = "P50225";
+	    if ( args.length == 1 )
+		ac = args[0];
+	    GetSequence s = new GetSequence();
+	    s.showExample(ac);
+
+	}
+
+	public void showExample(String ac) {
+		try {
+			// the sequence example is very similar to the structure example.
+			// a new Thread is created that does the DAS communication
+			// a Listener waits for the response.
+
+			// first we set some system properties
+
+			// make sure we use the Xerces XML parser..
+			System.setProperty("javax.xml.parsers.DocumentBuilderFactory",
+					"org.apache.xerces.jaxp.DocumentBuilderFactoryImpl");
+			System.setProperty("javax.xml.parsers.SAXParserFactory",
+					"org.apache.xerces.jaxp.SAXParserFactoryImpl");
+
+			// if you are behind a proxy, please uncomment the following lines
+			System.setProperty("proxySet", "true");
+			System.setProperty("proxyHost", "wwwcache.sanger.ac.uk");
+			System.setProperty("proxyPort", "3128");
+
+			// first let's create a SpiceDasSource which knows where the
+			// DAS server is located.
+
+			Das1Source dasSource = new Das1Source();
+
+			dasSource.setUrl("http://www.ebi.ac.uk/das-srv/uniprot/das/aristotle/");
+
+		
+
+			// now we create the thread that will fetch the structure
+			SequenceThread thread = new SequenceThread(ac, dasSource);
+
+			// add a structureListener that simply prints the PDB code
+			SequenceListener listener = new MyListener();
+			thread.addSequenceListener(listener);
+
+			// and now start the DAS request
+			thread.start();
+
+			// do an (almost) endless loop which is terminated in the StructureListener...
+			int i = 0;
+			while (true) {
+				
+				System.out.println(i + "/10th seconds have passed");
+				i++;
+				Thread.sleep(100);
+				if (i > 1000) {
+					System.err.println("something went wrong. Perhaps a proxy problem?");
+					System.exit(1);
+				}
+				
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	class MyListener implements SequenceListener {
+
+		/** this method is called when the Thread finishes 
+		 it prints out the sequence as in Fasta format
+		 */
+		public synchronized void newSequence(SequenceEvent event) {
+			String accessionCode = event.getAccessionCode();
+			String sequence = event.getSequence();
+			
+			System.out.println(">" + accessionCode);
+			System.out.println(sequence);
+			System.exit(0);
+		}
+
+		// the methods below are required by the interface but not needed here
+		public void newObjectRequested(String accessionCode) {
+		}
+
+		public void selectionLocked(boolean flag) {
+		}
+
+		public void selectedSeqRange(int start, int end) {
+		}
+
+		public void selectedSeqPosition(int position) {
+		}
+
+		public void clearSelection() {
+		}
+
+		public void noObjectFound(String accessionCode) {
+            System.out.println("did not find a sequence with accession code " + accessionCode );
+            System.exit(0);
+		}
+
+	}
+
+}
