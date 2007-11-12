4615f5b6165ce21ae9434fb08c61af0c546b9ac9
andreas <andreas@f893cd9c-26d5-0310-a8ce-8029f9ad61c0>
2007-11-12 17:18:53 +0000

    improving example

--- dasobert/branches/interactionDAS/doc/examples/GetInteraction.java
+++ dasobert/branches/interactionDAS/doc/examples/GetInteraction.java
@@ -23,35 +23,56 @@ import de.mpg.mpiinf.ag3.dasmi.model.Interaction;
 
 /**
  * Small test class showing the exemplary use of the DASMI extension
- * @author Hagen
+ * @author Hagen Blankenburg
  *
  */
 public class GetInteraction {
-	private static Map sources = new HashMap(); // the DAS sources, coming from the registry
-	private static DasCoordinateSystem queryCoordSys = null; // coordinate system of the query
-	private static int activeThreads = 0;
+	private  Map <String,InteractionDasSource>sources = new HashMap<String,InteractionDasSource>(); // the DAS sources, coming from the registry
+	
+	private  int activeThreads = 0;
 	
 	public static void main(String[] args) {
+		
 		System.setProperty("javax.xml.parsers.DocumentBuilderFactory", "org.apache.xerces.jaxp.DocumentBuilderFactoryImpl");
 		System.setProperty("javax.xml.parsers.SAXParserFactory", "org.apache.xerces.jaxp.SAXParserFactoryImpl");
+		
+//		 if you are behind a proxy, please uncomment the following lines
+	    System.setProperty("proxySet","true");
+	    System.setProperty("proxyHost","wwwcache.sanger.ac.uk");
+	    System.setProperty("proxyPort","3128");
+		
+		
+		GetInteraction me = new GetInteraction();
+		try {
+			me.getInteractionDasSources("http://dasmi.bioinf.mpi-inf.mpg.de/sources");
+		} catch (Exception e){
+			e.printStackTrace();
+			return;
+		}
 		// first load the XML file containing all the DAS sources from a DAS regisrtry
-		loadSources("http://dasmi.bioinf.mpi-inf.mpg.de/sources");
+		
 		// in this example we want to query bz an Entrez Gene_ID, so prepare the query coordinate system
+		DasCoordinateSystem
 		queryCoordSys = new DasCoordinateSystem();
 		queryCoordSys.setUniqueId("CS_DS16");
 		queryCoordSys.setCategory("Gene_ID");
 		queryCoordSys.setName("Entrez");
 		// and start the query
-		querySources("1212");
+		me.querySources("1212", queryCoordSys);
 	}
 	
+	public GetInteraction(){
+
+		
+	}
 	
 	/**
 	 * Retrieves the interactions from all the active sources. To this end starts an InteractionThread
 	 * for each source. The results of these threads will then be reported to the myListener class
 	 * @param query The query
+	 * @param queryCoordSys 
 	 */
-	public static void querySources(String query){
+	public void querySources(String query, DasCoordinateSystem queryCoordSys){
 		try {
 			// iterate over all interaction DAS sources 
 			Iterator it = sources.entrySet().iterator();
@@ -97,28 +118,14 @@ public class GetInteraction {
 	
 	
 	/**
-	 * Loads all the interaction sources from a DAS registry. 
-	 * @param registryUrl The url of the XML registry file, usually http://dasregistry.org/das1/sources
-	 */
-	public static void loadSources(String registryUrl){
-		try{
-			getInteractionDasSources(registryUrl);
-		}catch(Exception e){
-			e.printStackTrace();
-		}	
-	}
-	
-
-	
-	/**
-	 * Requests all the das1 sources from a DAS registry and stors those capable of the interaction extension.
+	 * Requests all the das1 sources from a DAS registry and stores those capable of the interaction extension.
 	 * 
 	 * @return
 	 * @throws MalformedURLException
 	 * @throws DASException
 	 */
-	private static void getInteractionDasSources(String registryUrl)throws MalformedURLException, DASException {
-		Set das1sources = new HashSet();
+	private  void getInteractionDasSources(String registryUrl)throws MalformedURLException, DASException {
+		Set <DasSource> das1sources = new HashSet<DasSource>();
 		//Map interactionDasSources = new HashMap();
 		DasSourceReaderImpl reader = new DasSourceReaderImpl();
 		URL url = new URL(registryUrl);
@@ -129,37 +136,18 @@ public class GetInteraction {
 		for (int i = 0; i < srcs.length; i++) {
 			DasSource ds = srcs[i];
 			// if it is a das2 source, check whether we can treat it as a das1 source
-			if (ds instanceof Das2Source) {
-				Das2Source d2s = (Das2Source) ds;
-				// if it can be treated as a das1 source
-				if (d2s.hasDas1Capabilities()) {
-					Das1Source d1s = DasSourceConverter.toDas1Source(d2s);
-					String[] caps = d1s.getCapabilities();
-					// now check whether it has interaction capabilities
-					boolean isInteractionSource = false;
-					for (int c = 0; c < caps.length; c++) {
-						if (caps[c].equals("interaction")) {
-							isInteractionSource = true;
-						}
-					}
-					// if it is, add it to the final list
-					if (isInteractionSource) {
-						das1sources.add(d1s);
-					}
-				}
-			} // if its a das1 source we only need to check whether its interaction capable
-			else if (ds instanceof Das1Source) {
-				String[] caps = ds.getCapabilities();
-				boolean isInteractionSource = false;
-				for (int c = 0; c < caps.length; c++) {
-					if (caps[c].equals("interaction")) {
-						isInteractionSource = true;
-					}
-				}
-				if (isInteractionSource) {
-					das1sources.add(ds);
+
+			String[] caps = ds.getCapabilities();
+			boolean isInteractionSource = false;
+			for (int c = 0; c < caps.length; c++) {
+				if (caps[c].equals("interaction")) {
+					isInteractionSource = true;
 				}
 			}
+			if (isInteractionSource) {
+				das1sources.add(ds);
+			}
+
 		}
 		
 		// convert the found sources to interaction sources and sort the QM sources out
@@ -183,7 +171,7 @@ public class GetInteraction {
 	 * @author Hagen
 	 * 
 	 */
-	private static class MyListener implements InteractionListener {
+	private class MyListener implements InteractionListener {
 		
 		/**
 		 * Called if a thread found interactions within its datasource
