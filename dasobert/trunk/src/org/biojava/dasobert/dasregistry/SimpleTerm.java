e99768b9e7e36b601c502c0c38dccdb9a58db672
jwarren <jwarren@f893cd9c-26d5-0310-a8ce-8029f9ad61c0>
2009-04-22 12:48:38 +0000

    this instead of biojava Term but may need to revert back for major code changes to enable more ontology tree searching features

---
+++ dasobert/trunk/src/org/biojava/dasobert/dasregistry/SimpleTerm.java
@@ -0,0 +1,91 @@
+package org.biojava.dasobert.dasregistry;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Very simple bean to keep information on an ontology term
+ * 
+ * @author jw12
+ * 
+ */
+public class SimpleTerm {
+
+	private String id = null;
+	private String name = null;
+	private String description = null;
+	private String synonyms[] = null;
+	private boolean isObsolete = false;
+	private HashMap<String, String> xrefs = null;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public HashMap<String, String> getXrefs() {
+		return xrefs;
+	}
+
+	public SimpleTerm(String id) {
+		this.id = id;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String[] getSynonyms() {
+		return synonyms;
+	}
+
+	public void setSynonyms(String[] synonyms) {
+		this.synonyms = synonyms;
+	}
+
+	public boolean isObsolete() {
+		return isObsolete;
+	}
+
+	public void setObsolete(boolean isObsolete) {
+		this.isObsolete = isObsolete;
+	}
+
+	public void setXrefs(HashMap<String,String> termXrefs) {
+		this.xrefs = termXrefs;
+
+	}
+
+	/**
+	 * debug method
+	 */
+	public void print() {
+		System.out.println("id:" + id + " name:" + name + " description:"
+				+ description + " isObselete:" + isObsolete);
+		Set<Map.Entry<String, String>> xrefValues = xrefs.entrySet();
+		Iterator<Map.Entry<String,String>> xrefIterator=xrefValues.iterator();
+		if (xrefs != null) {
+			Map.Entry<String, String>xref=xrefIterator.next();
+			String key=xref.getKey();
+			String value=xref.getValue();
+			System.out.println("key="+key+" value="+value);
+		}
+	}
+}
