0ca85c3cba0414b1d51d703f74afa4b123cc7761
jwarren <jwarren@f893cd9c-26d5-0310-a8ce-8029f9ad61c0>
2009-04-22 12:45:56 +0000

    added biojava blurb

--- src/org/biojava/dasobert/das/validation/RelaxNGValidatorMSV.java
+++ src/org/biojava/dasobert/das/validation/RelaxNGValidatorMSV.java
@@ -5,8 +5,24 @@ import java.util.Map;
 import org.biojava.dasobert.das.validation.RegistryRelaxNG;
 //import org.biojava.services.das.registry.RegistryConfiguration;
 
-/**
- * 
+/**                    BioJava development code
+*
+* This code may be freely distributed and modified under the
+* terms of the GNU Lesser General Public Licence.  This should
+* be distributed with the code.  If you do not have a copy,
+* see:
+*
+*      http://www.gnu.org/copyleft/lesser.html
+*
+* Copyright for this code is held jointly by the individual
+* authors.  These should be listed in @author doc comments.
+*
+* For more information on the BioJava project and its aims,
+* or to join the biojava-l mailing list, visit the home page
+* at:
+*
+*      http://www.biojava.org/
+*
  * @author jw12
  *
  */
@@ -15,8 +31,10 @@ public class RelaxNGValidatorMSV {
 	
 
 
-	private static String PATH = "http://www.dasregistry.org/validation/";//default also set here
-	
+
+	//private static String PATH = "http://localhost:8080/dasregistryOID/validation/";//default also set here
+	private static String PATH="http://localhost:8080/dasregistryOID/validation1.6E/";
+	//private static String PATH = "http://www.dasregistry.org/validation/";
 	public static final String INTERACTION = "interaction.rng";
 	public static final String SOURCES = "sources.rng";
 	public static final String SEQUENCE = "sequence.rng";
@@ -39,12 +57,21 @@ public class RelaxNGValidatorMSV {
 	private RegistryRelaxNG rng;
 	
 	public RelaxNGValidatorMSV(){
+
 		//RegistryConfiguration rconfig = new RegistryConfiguration();
+
 		// rconfig is set by the outside via Spring
+
+		//configuration = rconfig.getConfiguration();
+		//PATH=(String)configuration.get("relaxNgBasePath");
+		
+		System.out.println("setting relaxng base path in msv validator="+PATH);
+
 		//configuration = rconfig.getConfiguration();
 		
 		//PATH=(String)configuration.get("relaxNgBasePath");
 		//System.out.println("setting relaxng base path in msv validator="+PATH);
+
 		rng = new RegistryRelaxNG();
 	}
 
@@ -68,6 +95,6 @@ public class RelaxNGValidatorMSV {
 		return message;
 	}
 	
-	
+
 
 }
