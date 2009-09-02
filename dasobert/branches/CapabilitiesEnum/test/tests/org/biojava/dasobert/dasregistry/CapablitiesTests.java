02113d3a7c580b7cf04a4b2b2b282ebdb888d6f4
jwarren <jwarren@f893cd9c-26d5-0310-a8ce-8029f9ad61c0>
2009-09-02 16:48:28 +0000

    working but not validation messages

--- dasobert/branches/CapabilitiesEnum/test/tests/org/biojava/dasobert/dasregistry/CapablitiesTests.java
+++ dasobert/branches/CapabilitiesEnum/test/tests/org/biojava/dasobert/dasregistry/CapablitiesTests.java
@@ -1,6 +1,7 @@
 package tests.org.biojava.dasobert.dasregistry;
 import junit.framework.TestCase;
 import org.biojava.dasobert.das.Capabilities;
+import org.biojava.dasobert.das.NoSuchCapabilityException;
 
 
 public class CapablitiesTests extends TestCase{
@@ -11,10 +12,22 @@ public class CapablitiesTests extends TestCase{
 
 	public void testCapabilities(){
 		String[] strings={"sequence","sources"};
-		Capabilities[]caps=Capabilities.capabilitiesFromStrings(strings);
+		Capabilities[] caps=null;
+		//try {
+			caps = Capabilities.capabilitiesFromStrings(strings);
+		//} catch (NoSuchCapabilityException e) {
+			// TODO Auto-generated catch block
+		//	e.printStackTrace();
+		//}
 		assert(caps.length==strings.length);
 		String[]falsStrings={"sources","rubbish"};
-		Capabilities []caps2=Capabilities.capabilitiesFromStrings(falsStrings);
-		assertTrue(caps2.length<falsStrings.length);
+		Capabilities[] caps2=null;
+		//try {
+			caps2 = Capabilities.capabilitiesFromStrings(falsStrings);
+		//} catch (NoSuchCapabilityException e) {
+			// could change this to save a null pointer exception by creating an array if we want to
+			//e.printStackTrace();
+		//}
+		assertTrue(caps2==null);
 	}
 }
--- dasobert/branches/CapabilitiesEnum/test/tests/org/biojava/dasobert/dasregistry/Das1_6ValidatorTest.java
+++ dasobert/branches/CapabilitiesEnum/test/tests/org/biojava/dasobert/dasregistry/Das1_6ValidatorTest.java
@@ -1,5 +1,6 @@
 package tests.org.biojava.dasobert.dasregistry;
 
+import org.biojava.dasobert.dasregistry.Das1Source;
 import org.biojava.dasobert.dasregistry.Das1Validator;
 import org.biojava.dasobert.dasregistry.Das1_6Validator;
 
@@ -10,6 +11,7 @@ public class Das1_6ValidatorTest extends TestCase {
 	final String relaxng16="http://localhost:8080/dasregistry/validation1.6/";
 	final String testCode="P08487";
 	final String testURL="http://das.sanger.ac.uk/das/pfam/";
+	Das1_6Validator validator16;
 	public void setUp(){
 		
 		System.setProperty("proxySet", "true");
@@ -19,25 +21,59 @@ public class Das1_6ValidatorTest extends TestCase {
 	
 		System.setProperty("javax.xml.parsers.DocumentBuilderFactory", "org.apache.xerces.jaxp.DocumentBuilderFactoryImpl");
 		System.setProperty("javax.xml.parsers.SAXParserFactory", "org.apache.xerces.jaxp.SAXParserFactoryImpl");
-		
+		validator16=new Das1_6Validator();
+		validator16.setRelaxNgPath(relaxng16);
 //		
 		
 	}
 	
-	public void testSourcesResponse(){
-		Das1Validator validator=new Das1Validator();
-		validator.setRelaxNgPath(relaxng15);
-		assertTrue(validator.validateSourcesCmd("http://www.ensembl.org/das/sources"));
-		System.out.println(validator.getValidationMessage());
-		Das1_6Validator validator16=new Das1_6Validator();
-		validator16.setRelaxNgPath(relaxng16);
-		assertTrue(validator16.validateSourcesCmd("http://www.ensembl.org/das/sources")==validator.validateSourcesCmd("http://www.ensembl.org/das/sources"));
-		assertTrue(validator16.getValidationMessage()==validator.getValidationMessage());
-		
-		assertFalse(validator16.validateFeatures(testURL, testCode, false)==validator.validateFeatures(testURL, testCode, false));
-		assertFalse(validator16.validateSequence(testURL, testCode)==validator.validateSequence(testURL, testCode));
-		assertFalse(validator16.validateTypes(testURL, false)==validator.validateTypes(testURL, false));
+//	public void testSourcesResponse(){
+//		Das1Validator validator=new Das1Validator();
+//		validator.setRelaxNgPath(relaxng15);
+//		assertTrue(validator.validateSourcesCmd("http://www.ensembl.org/das/sources"));
+//		System.out.println(validator.getValidationMessage());
+//		Das1_6Validator validator16=new Das1_6Validator();
+//		validator16.setRelaxNgPath(relaxng16);
+//		assertTrue(validator16.validateSourcesCmd("http://www.ensembl.org/das/sources")==validator.validateSourcesCmd("http://www.ensembl.org/das/sources"));
+//		assertTrue(validator16.getValidationMessage()==validator.getValidationMessage());
+//		
+//		assertFalse(validator16.validateFeatures(testURL, testCode, false)==validator.validateFeatures(testURL, testCode, false));
+//		assertFalse(validator16.validateSequence(testURL, testCode)==validator.validateSequence(testURL, testCode));
+//		assertFalse(validator16.validateTypes(testURL, false)==validator.validateTypes(testURL, false));
+//		
+//	}
+	
+	public void testHeadersTesting(){
+		validator16.validateHeaders(testURL);
 		
 	}
+	
+	public void testAndy16SourcesResponse(){
+//		Das1Validator validator=new Das1Validator();
+//		validator.setRelaxNgPath(relaxng15);
+		//assertTrue(validator.validateSourcesCmd("http://www.ensembl.org/das/sources"));
+		//System.out.println(validator.getValidationMessage());
+		
+		String andyUrl="http://wwwdev.ebi.ac.uk/das-srv/genomicdas/das/sources";
+		
+		//assertTrue(validator16.validateSourcesCmd(andyUrl));
+		System.out.println("about to run getSources");
+		Das1Source [] sources=validator16.getDas1SourcesFromSourcesXml(andyUrl);
+		System.out.println("sources length:"+sources.length);
+
+		boolean allValid=validator16.validateSources(sources);
+		assertTrue(allValid);
+		if(allValid)System.out.println("-------------All sources are valid");
+		
+//		assertTrue(validator16.validateSourcesCmd("http://www.ensembl.org/das/sources")==validator.validateSourcesCmd("http://www.ensembl.org/das/sources"));
+//		assertTrue(validator16.getValidationMessage()==validator.getValidationMessage());
+//		
+//		assertFalse(validator16.validateFeatures(testURL, testCode, false)==validator.validateFeatures(testURL, testCode, false));
+//		assertFalse(validator16.validateSequence(testURL, testCode)==validator.validateSequence(testURL, testCode));
+//		assertFalse(validator16.validateTypes(testURL, false)==validator.validateTypes(testURL, false));
+		
+	}
+	
+	
 
 }
